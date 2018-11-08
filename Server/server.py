2d5430f3d72f317332274d663ae59f4cd2ae4b5e
Wonho ha <haho013039@gmail.com>
2018-11-08 19:36:18 +0900

    Update Server/server.py

---
+++ Server/server.py
@@ -0,0 +1,13 @@
+from flask import Flask
+import numpy as np
+
+app = Flask(__name__)
+
+@app.route('/random')
+def home():
+    random = np.random.randint(10)
+    return random
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
